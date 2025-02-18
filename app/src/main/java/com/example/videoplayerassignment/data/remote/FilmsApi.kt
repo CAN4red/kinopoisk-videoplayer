d8f7c838f4a85f8a556f81560e7a0f6d610cf24f
can4red <CAN4red@yandex.ru>
2025-02-18 17:14:17 +0300

    FilmsApi added

---
+++ app/src/main/java/com/example/videoplayerassignment/data/remote/FilmsApi.kt
@@ -0,0 +1,19 @@
+package com.example.videoplayerassignment.data.remote
+
+import com.example.videoplayerassignment.data.remote.dto.FilmsList
+import com.example.videoplayerassignment.data.remote.dto.FilmsListInfo
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface FilmsApi {
+
+    @GET("/api/v2.2/films/collections?type=$COLLECTION_TYPE&page=1")
+    suspend fun getFilmsListInfo(): FilmsListInfo
+
+    @GET("/api/v2.2/films/collections?type=$COLLECTION_TYPE&page={page}")
+    suspend fun getFilmsList(@Path("page") page: Int): FilmsList
+
+    companion object {
+        private const val COLLECTION_TYPE = "TOP_POPULAR_MOVIES"
+    }
+}
