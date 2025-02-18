0149b0b4a596003f69ffed1c91ab7ec19be82c74
can4red <CAN4red@yandex.ru>
2025-02-18 22:15:11 +0300

    UseCases added

---
+++ app/src/main/java/com/example/videoplayerassignment/domain/use_case/GetFilmListInfoUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.videoplayerassignment.domain.use_case
+
+import com.example.videoplayerassignment.common.Resource
+import com.example.videoplayerassignment.data.remote.dto.FilmListInfo
+import com.example.videoplayerassignment.domain.repository.FilmRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetFilmListInfoUseCase @Inject constructor(
+    private val repository: FilmRepository
+) {
+    operator fun invoke(): Flow<Resource<FilmListInfo>> {
+        return repository.getFilmsListInfo()
+    }
+}
---
+++ app/src/main/java/com/example/videoplayerassignment/domain/use_case/GetNewFilmPagesUseCase.kt
@@ -0,0 +1,50 @@
+package com.example.videoplayerassignment.domain.use_case
+
+import com.example.videoplayerassignment.common.Constants
+import com.example.videoplayerassignment.common.Resource
+import com.example.videoplayerassignment.data.remote.dto.Film
+import com.example.videoplayerassignment.data.remote.dto.FilmListInfo
+import com.example.videoplayerassignment.domain.repository.FilmRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+class GetNewFilmPagesUseCase @Inject constructor(
+    private val repository: FilmRepository
+) {
+    operator fun invoke(
+        currentFilms: List<Film>,
+        filmListInfo: FilmListInfo
+    ): Flow<Resource<List<Film>>> {
+
+        val currentPage = currentFilms.size / Constants.PAGE_LENGTH
+        val nextPage = currentPage + 1
+
+        if (nextPage > filmListInfo.totalPages) {
+            return flow {
+                emit(Resource.Success<List<Film>>(currentFilms))
+            }
+        }
+
+        return repository.getFilmListByPage(nextPage).onEach { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    Resource.Loading<List<Film>>(data = currentFilms)
+                }
+
+                is Resource.Success -> {
+                    val combinedList = currentFilms + result.data
+                    Resource.Success<List<Film>>(combinedList)
+                }
+
+                is Resource.Error -> {
+                    Resource.Error<List<Film>>(
+                        message = result.message,
+                        data = currentFilms,
+                    )
+                }
+            }
+        }
+    }
+}
