b08c9956a0cfda7b3650ab8a7fadc823c6415d32
can4red <CAN4red@yandex.ru>
2025-02-19 15:48:18 +0300

    Dao added

---
+++ app/src/main/java/com/example/videoplayerassignment/data/local/dao/FilmItemDao.kt
@@ -0,0 +1,30 @@
+package com.example.videoplayerassignment.data.local.dao
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.example.videoplayerassignment.data.local.entities.CountryEntity
+import com.example.videoplayerassignment.data.local.entities.FilmItemEntity
+import com.example.videoplayerassignment.data.local.entities.GenreEntity
+import com.example.videoplayerassignment.data.local.relations.FilmItemWithCountriesAndGenresEntity
+
+interface FilmDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFilm(film: FilmItemEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCountry(country: CountryEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertGenre(genre: GenreEntity)
+
+    @Transaction
+    @Query("DELETE FROM films")
+    suspend fun deleteAllFilms()
+
+    @Transaction
+    @Query("SELECT * FROM films")
+    suspend fun getAllFilms(): List<FilmItemWithCountriesAndGenresEntity>
+}
--- app/src/main/java/com/example/videoplayerassignment/data/local/entities/FilmItemEntity.kt
+++ app/src/main/java/com/example/videoplayerassignment/data/local/entities/FilmItemEntity.kt
@@ -1,9 +1,11 @@
 package com.example.videoplayerassignment.data.local.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "films")
 data class FilmItemEntity(
+    @PrimaryKey(autoGenerate = false)
     val id: Int,
     val name: String,
     val year: String,
