a2357c1052fd565967f4a3800f7229d24d9c1860
can4red <CAN4red@yandex.ru>
2025-02-19 13:18:40 +0300

    FilmListScreen added

--- app/src/main/java/com/example/videoplayerassignment/presentation/film_list/FilmListScreen.kt
+++ app/src/main/java/com/example/videoplayerassignment/presentation/film_list/FilmListScreen.kt
@@ -1,4 +1,27 @@
 package com.example.videoplayerassignment.presentation.film_list
 
-class FilmListScreen {
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.videoplayerassignment.presentation.film_list.components.FilmList
+
+@Composable
+fun FilmListScreen(
+    modifier: Modifier = Modifier,
+    viewModel: FilmListViewModel = hiltViewModel()
+) {
+    val state by viewModel.state.collectAsState()
+
+    Log.i("Film List Update", state.films.size.toString())
+
+    FilmList(
+        films = state.films,
+        onFilmItemClick = { },
+        loadMoreFilms = viewModel::loadMoreFilms,
+        modifier = modifier.fillMaxSize()
+    )
 }
